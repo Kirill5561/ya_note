2af20feeb8c411f6d1ba1ae2b41a7d330ec72245
Kirill Sidorov <ksidorov2018@yandex.ru>
2023-11-05 01:54:35 +0400

    Add new file

---
+++ notes/pytest_tests/test_content.py
@@ -0,0 +1,23 @@
+from django.urls import reverse
+
+# В тесте используем фикстуру заметки
+# и фикстуру клиента с автором заметки.
+def test_note_in_list_for_author(note, author_client):
+    url = reverse('notes:list')
+    # Запрашиваем страницу со списком заметок:
+    response = author_client.get(url)
+    # Получаем список объектов из контекста:
+    object_list = response.context['object_list']
+    # Проверяем, что заметка находится в этом списке:
+    assert note in object_list
+
+
+# В этом тесте тоже используем фикстуру заметки,
+# но в качестве клиента используем admin_client;
+# он не автор заметки, так что заметка не должна быть ему видна.
+def test_note_not_in_list_for_another_user(note, admin_client):
+    url = reverse('notes:list')
+    response = admin_client.get(url)
+    object_list = response.context['object_list']
+    # Проверяем, что заметки нет в контексте страницы:
+    assert note not in object_list
